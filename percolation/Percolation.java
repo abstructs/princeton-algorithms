8612e6b874bbd9da40541f638365c6017f0e7bb2
abstructs <andrewclarke540@gmail.com>
2018-11-19 21:56:22 -0500

    implement perculation algorithm

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -9,19 +9,50 @@ public class Percolation {
         }
     }
 
+    private void printSizes() {
+        for(int i = 0; i < sizes.length; i++) {
+            for (int j = 0; j < sizes.length; j++) {
+                System.out.print(String.format("%d  ", sizes[i][j]));
+            }
+            System.out.println("\n");
+        }
+    }
+
     public static void main(String[] args) {
-        Percolation perc = new Percolation(2);
+        Percolation perc = new Percolation(4);
 
         perc.open(0, 1);
 
         perc.open(1, 1);
 
-        perc.open(1, 0);
+        perc.open(3, 2);
+
+        perc.open(2, 2);
+
+//        perc.open(1, 0);
 
         perc.open(0, 0);
 
+//        perc.open(3, 3);
+        perc.open(2, 3);
+        perc.open(1, 3);
 
+        perc.open(1, 2);
+
+//        perc.open(3, 1);
+
+//        perc.open(3, 0);
+//
+//        perc.open(3, 1);
+//        perc.open(3, 2);
+
+//        System.out.println(perc.size(2));
+
+//        perc.open(0, 0);
+
+//        perc.printSizes();
         perc.print();
+        System.out.println(perc.percolates());
     }
 
     private int[][] elements;
@@ -39,21 +70,24 @@ public class Percolation {
 
     }
 
-    public int index(int row, int col) {
+    private int index(int row, int col) {
         return elements.length * row + col;
     }
 
-    public int deindexCol(int index) {
+    private int deindexCol(int index) {
         return index % elements.length;
     }
 
-    public int deindexRow(int index) {
+    private int deindexRow(int index) {
         return (index - deindexCol(index)) / elements.length;
     }
 
     private int root(int index) {
         int parentIndex = index;
         int i = deindexRow(index), j = deindexCol(index);
+
+        System.out.println(i);
+        System.out.println(j);
 
         while(parentIndex != elements[i][j]) {
             parentIndex = elements[i][j];
@@ -64,17 +98,17 @@ public class Percolation {
         return parentIndex;
     }
 
-//    private int size(int row, int col) {
-//        int rootIndex = root(row, col);
-//        int rootRow = deindexRow(rootIndex), rootCol = deindexCol(rootIndex);
-//
-//        return sizes[rootRow][rootCol];
-//    }
+    private int root(int row, int col) {
+        int parentIndex = index(row, col);
+        int i = row, j = col;
 
-    private int size(int index) {
-        int row = deindexRow(index), col = deindexCol(index);
+        while(parentIndex != elements[i][j]) {
+            parentIndex = elements[i][j];
+            i = deindexRow(parentIndex);
+            j = deindexCol(parentIndex);
+        }
 
-        return sizes[row][col];
+        return parentIndex;
     }
 
     private void setElement(int index, int ele) {
@@ -97,27 +131,37 @@ public class Percolation {
         return elements[row][col];
     }
 
-    private boolean inBounds(int index) {
-        int col = deindexCol(index), row = deindexRow(index);
+    private boolean inBounds(int row, int col) {
         return (row >= 0 && row < elements.length) && (col >= 0 && col < elements.length);
     }
 
-    private void union(int leftIndex, int rightIndex) {
+    private boolean connected(int leftIndex, int rightIndex) {
         int leftRoot = root(leftIndex), rightRoot = root(rightIndex);
-        int leftRootSize = size(leftRoot), rightRootSize = size(rightRoot);
+        return leftRoot == rightRoot;
+    }
 
-        if(leftRootSize < rightRootSize) {
+    private void union(int leftIndex, int rightIndex) {
+        if(connected(leftIndex, rightIndex))
+            return;
+
+        int leftRoot = root(leftIndex), rightRoot = root(rightIndex);
+        int leftRootSize = getSize(leftRoot), rightRootSize = getSize(rightRoot);
+
+        if(leftRootSize >= rightRootSize) {
             setElement(rightRoot, leftRoot);
-            setSize(leftRoot, leftRootSize + rightRootSize);
+            setSize(leftRoot, 1 + rightRootSize);
         } else {
             setElement(leftRoot, rightRoot);
-            setSize(rightRoot, rightRootSize + leftRootSize);
+            setSize(rightRoot, 1 + leftRootSize);
         }
-
     }
 
     // open site (row, col) if it is not open already
     public void open(int row, int col) {
+        // grader starts arrays at 1 (ew)
+        row--;
+        col--;
+
         elements[row][col] = index(row, col);
         sizes[row][col] = 1;
 
@@ -125,20 +169,24 @@ public class Percolation {
         int leftIndex = index(row, col - 1), rightIndex = index(row, col + 1);
         int upIndex = index(row - 1, col), downIndex = index(row + 1, col);
 
-        // check boundaries
-//        if(isOpen())
-        if(inBounds(leftIndex) && isOpen(leftIndex))
+        if(inBounds(row, col - 1) && isOpen(leftIndex))
             union(index, leftIndex);
-        if(inBounds(rightIndex) && isOpen(rightIndex))
+        if(inBounds(row, col + 1) && isOpen(rightIndex))
             union(index, rightIndex);
-        if(inBounds(upIndex) && isOpen(upIndex))
+        if(inBounds(row - 1, col) && isOpen(upIndex))
             union(index, upIndex);
-        if(inBounds(downIndex) && isOpen(downIndex))
+        if(inBounds(row + 1, col) && isOpen(downIndex))
             union(index, downIndex);
     }
 
     // is site (row, col) open?
+    // row and col starts at 1
     public boolean isOpen(int row, int col) {
+        row--;
+        col--;
+        if(!inBounds(row, col))
+            return false;
+
         return elements[row][col] >= 0;
     }
 
@@ -149,15 +197,24 @@ public class Percolation {
 
     // is site (row, col) full?
     public boolean isFull(int row, int col) {
-//        return elements[row][col] >= 0;
+        if(!inBounds(row - 1, col - 1) || !isOpen(row, col))
+            return false;
+
+        int rootIndex = root(index(row - 1, col - 1));
+        for(int j = 1; j <= elements.length; j++) {
+            if(isOpen(1, j) && connected(rootIndex, root(0, j - 1))) {
+                return true;
+            }
+        }
+
         return false;
     }
 
     // number of open sites
     public int numberOfOpenSites() {
         int count = 0;
-        for(int i = 0; i < elements.length; i++)
-            for(int j = 0; j < elements.length; j++)
+        for(int i = 1; i <= elements.length; i++)
+            for(int j = 1; j <= elements.length; j++)
                 if(isOpen(i, j))
                     count++;
 
@@ -165,6 +222,14 @@ public class Percolation {
     }
     // does the system percolate?
     public boolean percolates() {
+        for(int i = 0; i < elements.length; i++) {
+            for(int j = 0; j < elements.length; j++) {
+                int topIndex = index(0, i), bottomIndex = index(elements.length - 1, j);
+
+                if(isOpen(topIndex) && isOpen(bottomIndex) && connected(topIndex, bottomIndex))
+                    return true;
+            }
+        }
 
         return false;
     }
